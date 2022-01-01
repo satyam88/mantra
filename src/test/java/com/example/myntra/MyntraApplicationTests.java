1803163921591493196f2ebe44d02fb8cc7e75a6
satyam88 <satyampandey@gmail.com>
2022-01-01 09:47:48 +0530

    first commit

---
+++ src/test/java/com/example/myntra/MyntraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myntra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyntraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
